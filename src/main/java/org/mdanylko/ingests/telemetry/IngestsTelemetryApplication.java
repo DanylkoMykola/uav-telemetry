dbae9849f607b47a215dbf8991a785c44f0f1fe7
mdanylk <mdanylko@agileassets.com>
2025-09-18 23:04:39 +0300

    Add proto deffenition

---
+++ src/main/java/org/mdanylko/ingests/telemetry/IngestsTelemetryApplication.java
@@ -0,0 +1,13 @@
+package org.mdanylko.ingests.telemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestsTelemetryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngestsTelemetryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mdanylko/ingests/telemetry/IngestsTelemetryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mdanylko.ingests.telemetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngestsTelemetryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
