7a493fd0a1643df416183c9381c62056e19b1b08
mdanylk <mdanylko@agileassets.com>
2026-04-26 17:34:47 +0300

    update terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "s3" {
+    bucket         = "REPLACE_WITH_YOUR_S3_BUCKET_NAME"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
